a7d8b9cbf9ff27f949ba2f634a803dbf5bebe752
VuNguyen <vu.nguyentuan@2359media.com.vn>
2014-07-16 15:45:05 +0700

    attach conversation model to app models

--- app/models.js
+++ app/models.js
@@ -2,4 +2,6 @@ exports = module.exports = function(app) {
   app.db.models.oauth = require('./models/oauth');
   app.db.models.User = require('./models/user');
   app.db.models.OAuthClientsModel = require('./models/oauth_client');
+  app.db.models.Conversation = require('./models/conversation');
+  app.db.models.Message = require('./models/message');
 };
