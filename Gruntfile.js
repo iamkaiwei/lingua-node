38895566695b287ad982be2db69ad1d5572f4aa1
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-07-23 12:31:33 +0700

    change Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/lingua_development');
 
 var User = require('./app/models/user')
-  , OAuthClientsModel = require('./app/models/oauth_client')
   , Conversation = require('./app/models/conversation')
+  , OAuthClientsModel = require('./app/models/oauth_client')
   , MessageType = require('./app/models/message_type');
 
 module.exports = function(grunt) {
@@ -11,16 +11,12 @@ module.exports = function(grunt) {
     //invoke async mode
     var done = this.async();
 
-    OAuthClientsModel.collection.remove(function() {    
-      console.log("Empty oauth_clients collection");
+    User.collection.remove(function() {
+      console.log("Empty users collection");
 
-      User.collection.remove(function() {
-        console.log("Empty users collection");
-
-        Conversation.collection.remove(function() {
-          console.log("Empty conversations collection");
-          done();
-        });
+      Conversation.collection.remove(function() {
+        console.log("Empty conversations collection");
+        done();
       });
     });
   });
@@ -28,7 +24,20 @@ module.exports = function(grunt) {
   grunt.registerTask('db-init', function() {
     var done = this.async();
 
-    MessageType.create(require('./sample-data/message_types.json'))
+    OAuthClientsModel.create({
+      clientId: 'lingua-ios',
+      clientSecret: 'l1n9u4',
+      redirectUri: '/oauth/redirect'
+    })
+    .then(
+      function() {
+        console.log('Inserted all clients');
+        return MessageType.create(require('./sample_data/message_types.json'));
+      },
+      function(err) {
+        console.log(err);
+        done(false);
+    })
     .then(
       function() {
         console.log('Inserted all message types');
@@ -43,24 +52,11 @@ module.exports = function(grunt) {
   grunt.registerTask('db-seed', function() {
     var done = this.async();
 
-    OAuthClientsModel.create({
-      clientId: 'lingua-ios',
-      clientSecret: 'l1n9u4',
-      redirectUri: '/oauth/redirect'
-    })
-    .then(
-      function() {
-        console.log('Inserted all clients');
-        return User.create(require('./sample-data/users.json'));
-      },
-      function(err) {
-        console.log(err);
-        done(false);
-    })
+    User.create(require('./sample_data/users.json'))
     .then(
       function() {
         console.log('Inserted all users');
-        return Conversation.create(require('./sample-data/conversations.json'));
+        return Conversation.create(require('./sample_data/conversations.json'));
       },
       function(err) {
         console.log(err);
