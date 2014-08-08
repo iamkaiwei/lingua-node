d1402d81041ca03671e9bca47fb431d2bd1effee
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-08-09 02:07:30 +0700

    change new messages API, must post an array of messages

--- app/controllers/api/v1/messages.js
+++ app/controllers/api/v1/messages.js
@@ -20,7 +20,7 @@ exports.list = function(req, res){
         )
         .skip(length*(page - 1))
         .limit(length)
-        .sort({ created_at: -1 })
+        .sort({ _id: -1 })
         .populate('message_type_id', 'name')
         .exec(function(err, messages){
           if (!err) {
@@ -37,25 +37,52 @@ exports.list = function(req, res){
  * @return {JSON} message
  */
 exports.create = function(req, res){
-  var newMessage = new res.app.db.models.Message(),
-    b = req.body,
-    p = req.params;
+  var b = req.body,
+    p = req.params,
+    messagesArray = b.map(function(message){
+      message.conversation_id = p.conversation_id;
+      return message;
+    });
 
-  newMessage.conversation_id = p.conversation_id;
-  newMessage.sender_id = b.sender_id;
-  newMessage.message_type_id = b.message_type_id;
-  newMessage.content = b.content;
-
-  newMessage.save(function(err, message){
+  res.app.db.models.Message.create(messagesArray, function(err){
     if (!err) {
+      var messageIds = Array.prototype.slice.call(arguments, 0)
+                        .slice(1)
+                        .reverse()
+                        .map(function(message){
+                          return message._id;
+                        });
+
       req.app.db.models.Conversation.findById(
         p.conversation_id,
         function(err, conversation){
-          conversation.messages.unshift(message._id);
+          conversation.messages = messageIds.concat(conversation.messages);
           conversation.save(function(){
-            res.send(message);
+            res.send(conversation.messages);
           });
         });
     }
   });
+
+  // var newMessage = new res.app.db.models.Message(),
+  //   b = req.body,
+  //   p = req.params;
+
+  // newMessage.conversation_id = p.conversation_id;
+  // newMessage.sender_id = b.sender_id;
+  // newMessage.message_type_id = b.message_type_id;
+  // newMessage.content = b.content;
+
+  // newMessage.save(function(err, message){
+  //   if (!err) {
+  //     req.app.db.models.Conversation.findById(
+  //       p.conversation_id,
+  //       function(err, conversation){
+  //         conversation.messages.unshift(message._id);
+  //         conversation.save(function(){
+  //           res.send(message);
+  //         });
+  //       });
+  //   }
+  // });
 };
