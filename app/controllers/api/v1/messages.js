4ef97bc1448e87f5a5ce77a234dc26faf19cd75a
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-08-09 10:42:17 +0700

    revert to the old create message API

--- app/controllers/api/v1/messages.js
+++ app/controllers/api/v1/messages.js
@@ -20,7 +20,7 @@ exports.list = function(req, res){
         )
         .skip(length*(page - 1))
         .limit(length)
-        .sort({ _id: -1 })
+        .sort({ created_at: -1 })
         .populate('message_type_id', 'name')
         .exec(function(err, messages){
           if (!err) {
@@ -37,52 +37,53 @@ exports.list = function(req, res){
  * @return {JSON} message
  */
 exports.create = function(req, res){
-  var b = req.body,
-    p = req.params,
-    messagesArray = b.map(function(message){
-      message.conversation_id = p.conversation_id;
-      return message;
-    });
+  // var b = req.body,
+  //   p = req.params,
+  //   messagesArray = b.map(function(message){
+  //     message.conversation_id = p.conversation_id;
+  //     return message;
+  //   });
 
-  res.app.db.models.Message.create(messagesArray, function(err){
-    if (!err) {
-      var messageIds = Array.prototype.slice.call(arguments, 0)
-                        .slice(1)
-                        .reverse()
-                        .map(function(message){
-                          return message._id;
-                        });
-
-      req.app.db.models.Conversation.findById(
-        p.conversation_id,
-        function(err, conversation){
-          conversation.messages = messageIds.concat(conversation.messages);
-          conversation.save(function(){
-            res.send(conversation.messages);
-          });
-        });
-    }
-  });
-
-  // var newMessage = new res.app.db.models.Message(),
-  //   b = req.body,
-  //   p = req.params;
-
-  // newMessage.conversation_id = p.conversation_id;
-  // newMessage.sender_id = b.sender_id;
-  // newMessage.message_type_id = b.message_type_id;
-  // newMessage.content = b.content;
-
-  // newMessage.save(function(err, message){
+  // res.app.db.models.Message.create(messagesArray, function(err){
   //   if (!err) {
+  //     var messageIds = Array.prototype.slice.call(arguments, 0)
+  //                       .slice(1)
+  //                       .reverse()
+  //                       .map(function(message){
+  //                         return message._id;
+  //                       });
+
   //     req.app.db.models.Conversation.findById(
   //       p.conversation_id,
   //       function(err, conversation){
-  //         conversation.messages.unshift(message._id);
+  //         conversation.messages = messageIds.concat(conversation.messages);
   //         conversation.save(function(){
-  //           res.send(message);
+  //           res.send(conversation.messages);
   //         });
   //       });
   //   }
   // });
+
+  var newMessage = new res.app.db.models.Message(),
+    b = req.body,
+    p = req.params;
+
+  newMessage.conversation_id = p.conversation_id;
+  newMessage.sender_id = b.sender_id;
+  newMessage.message_type_id = b.message_type_id;
+  newMessage.content = b.content;
+  newMessage.created_at = b.created_at;
+
+  newMessage.save(function(err, message){
+    if (!err) {
+      req.app.db.models.Conversation.findById(
+        p.conversation_id,
+        function(err, conversation){
+          conversation.messages.unshift(message._id);
+          conversation.save(function(){
+            res.send(message);
+          });
+        });
+    }
+  });
 };
